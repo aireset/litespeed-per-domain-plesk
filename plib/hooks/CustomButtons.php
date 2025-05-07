dd0ae3ab96f6a7869653d717caf766adcabcc721
Felipe Almeman <4226997+zhiru@users.noreply.github.com>
2025-05-07 01:20:40 +0000

    1.5.1

--- plib/hooks/CustomButtons.php
+++ plib/hooks/CustomButtons.php
@@ -15,6 +15,7 @@ class Modules_ToggleLitespeed_CustomButtons extends pm_Hook_CustomButtons
 
         // Pega o ID do domínio que o Plesk passa no GET
         $domainId = isset($_GET['domainId']) ? (int)$_GET['domainId'] : null;
+        var_dump($domainId);
         if ($domainId) {
             // Carrega o domínio e nome
             $domain = pm_Domain::getById($domainId);
@@ -41,6 +42,34 @@ class Modules_ToggleLitespeed_CustomButtons extends pm_Hook_CustomButtons
                 'link'        => $link,
                 'icon'        => pm_Context::getBaseUrl() . 'images/icon.svg',
             ];
+
+            $buttons[] = [
+                'place' => self::PLACE_DOMAIN_PROPERTIES_DYNAMIC,
+                'section' => self::SECTION_DOMAIN_PROPS_DYNAMIC_DEV_TOOLS,
+                'title'       => $title,
+                'description' => "$title neste domínio",
+                'link'        => $link,
+                'icon'        => pm_Context::getBaseUrl() . 'images/icon.svg',
+            ];
+        } else{
+
+            $buttons[] = [
+                'place'       => self::PLACE_COMMON,
+                'title'       => 'teste',
+                'description' => "treste neste domínio",
+                'link'        => '',
+                'icon'        => pm_Context::getBaseUrl() . 'images/icon.svg',
+            ];
+
+            $buttons[] = [
+                'place' => self::PLACE_DOMAIN_PROPERTIES_DYNAMIC,
+                'section' => self::SECTION_DOMAIN_PROPS_DYNAMIC_DEV_TOOLS,
+                'title'       => 'teste',
+                'description' => "treste neste domínio",
+                'link'        => '',
+                'icon'        => pm_Context::getBaseUrl() . 'images/icon.svg',
+            ];
+
         }
 
         return $buttons;
